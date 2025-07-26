98d9c1847977ca7a8bf74fade3b6c400d062fd35
Pietro Allesina <pietro.allesina@gmail.com>
2025-07-26 22:08:59 +0200

    Added "testdata/" folder and "populatedb.sql" script

---
+++ populatedb.sql
@@ -0,0 +1,5 @@
+use PortaAperta;
+load data local infile 'testdata/Operatori.txt' into table Operatori;
+load data local infile 'testdata/Clienti.txt' into table Clienti;
+load data local infile 'testdata/Prenotazioni.txt' into table Prenotazioni;
+load data local infile 'testdata/Accessi.txt' into table Accessi;
