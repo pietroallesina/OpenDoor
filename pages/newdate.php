65e3be8f4eea11608b01be20a2b73440726bf3d1
Pietro Allesina <pietro.allesina@gmail.com>
2025-08-14 19:59:01 +0200

    Added forms for searching for user and booking a date.

--- pages/newdate.php
+++ pages/newdate.php
@@ -1,14 +1,45 @@
 <?php
 require_once __DIR__ . '/../includes/init.php';
+require_once __DIR__ . '/../classes/Cliente.php';
+require_once __DIR__ . '/../classes/Prenotazione.php';
 
-function prenotazione(string $cognome, string $nome, string $regione, int $famigliari, string &$msg): void
+function trova_clienti(string $cognome, string $nome, string &$msg) { // restituisce array di oggetti Cliente
+    $clienti = [];
+    try {
+        $mysqli = new mysqli("mysql", "root", "", "OpenDoor");
+        $query = "CALL procedura_restituisci_dati_cliente(?, ?)";
+        $params = [$cognome, $nome];
+
+        $result = $mysqli->execute_query($query, $params);
+
+        for ($i = 0; $i < $result->num_rows; $i++) {
+            $row = $result->fetch_assoc();
+            $clienti[$i] = new Cliente($row['ID'], $cognome, $nome, $row['Regione'], $row['NumeroFamigliari'], $row['AccessiDisponibili'], $row['CreditiDisponibili']);
+        }
+
+    } catch (Exception $e) {
+        $msg = $e->getMessage();
+        if (isset($mysqli)) {
+            $mysqli->close();
+        }
+        return;
+    }
+
+    $mysqli->close();
+    $msg = 'Clienti trovati';
+    return $clienti;
+}
+
+function inserisci_prenotazione(int $IDcliente, string $data, int $crediti, int $limite, string &$msg): void
 {
     try {
         $mysqli = new mysqli("mysql", "root", "", "OpenDoor");
 
         $query = "CALL procedura_inserimento_prenotazione(?, ?, ?, ?)";
-        $params = [$cognome, $nome, $regione, $famigliari];
+        $params = [$IDcliente, $_SESSION['operatore']->ID(), $data, $crediti];
+
         $mysqli->execute_query($query, $params);
+
     } catch (Exception $e) {
         $msg = $e->getMessage();
         if (isset($mysqli)) {
@@ -29,13 +60,17 @@ if (!isset($_SESSION['operatore'])) {
     exit();
 }
 
+$clienti = [];
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $msg = '';
-    $cognome = $_POST['cognome'];
-    $nome = $_POST['nome'];
-    $regione = $_POST['regione'];
-    $famigliari = $_POST['famigliari'];
-    prenotazione($cognome, $nome, $regione, $famigliari, $msg);
+    $msg = "";
+    if (isset($_POST["trova-cliente"])) {
+        $clienti = trova_clienti($_POST["cognome"], $_POST["nome"], $msg);
+        // return;
+    }
+    if (isset($_POST["prenota"])) {
+        $limite = 100; // dummy value
+        inserisci_prenotazione($_POST["IDcliente"], $_POST["giorno"], $_POST["crediti"], $limite, $msg);
+    }
 }
 ?>
 
@@ -47,11 +82,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 
 <body>
-    <main>
-        <h1>Nuovo utente</h1>
+    <main class="newdate-wrapper">
 
-        <section class="access-form"> <!-- not access but still -->
-            <form method="post" action="" class="access-data">
+        <section class="trova-cliente">
+            <form method="post" action="">
                 <label for="nome">Nome:</label>
                 <input type="text" id="nome" name="nome" required>
                 <br>
@@ -60,19 +94,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <input type="text" id="cognome" name="cognome" required>
                 <br>
 
-                <label for="regione">Regione:</label>
-                <select id="regione" name="regione" required>
-                    <option value="ITA">Italia</option>
-                    <option value="PAK">Pakistan</option>
-                    <option value="AN">Altro</option>
+                <button type="submit" name="trova-cliente"> Trova </button>
+            </form>
+            <br>
+
+        </section class="trova-cliente">
+
+        <section class="prenota">
+            <form method="post" action="">
+                <label for="cliente"> Utenti trovati: </label>
+                <select id="cliente" name="IDcliente" required>
+                    <?php
+                        foreach ($clienti as $cliente) {
+                            echo "<option value='{$cliente->ID()}'> {$cliente->regione()} - {$cliente->ID()} </option>";
+                        }
+                    ?>
                 </select>
                 <br>
 
-                <label for="famigliari">Famigliari (almeno 1):</label>
-                <input type="number" id="famigliari" name="famigliari" min="1" required>
+                <label for="giorno"> Giorno: </label>
+                <input type="date" id="giorno" name="giorno" required>
                 <br>
 
-                <input type="submit" value="Inserisci">
+                <!-- [LATER] Orario -->
+
+                <label for="crediti"> Crediti: </label>
+                <input type="number" id="crediti" name="crediti" required>
+                <br>
+
+                <button type="submit" name="prenota"
+                    <?php echo empty($clienti) ? 'disabled' : ''; ?>
+                > Prenota
+                </button>
             </form>
             <br>
 
@@ -81,10 +134,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <br>
             <?php endif; ?>
 
-            <br>
-            <a class="exit-button" onclick="window.close();"> Chiudi finestra </a>
+        </section class="prenota">
 
-        </section>
+        <a class="exit-button" onclick="window.close();"> Chiudi finestra </a>
 
     </main>
 </body>
